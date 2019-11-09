dd5def06a02a390a5753d76dec05a72706167e0e
DengFa <103509468@qq.com>
2019-11-09 20:05:42 +0800

    fix

--- app/src/main/java/com/vincent/projectanalysis/activity/PhotoviewActivity.java
+++ app/src/main/java/com/vincent/projectanalysis/activity/PhotoviewActivity.java
@@ -2,7 +2,6 @@ package com.vincent.projectanalysis.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 import com.vincent.projectanalysis.R;
 import com.vincent.projectanalysis.widgets.photoviewextend.PhotoView;
@@ -15,6 +14,5 @@ public class PhotoviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photoview);
 
         PhotoView pv = findViewById(R.id.pv);
-        pv.setScaleType(ImageView.ScaleType.CENTER_CROP);
     }
 }
